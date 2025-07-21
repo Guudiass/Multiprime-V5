50f0dfb3b0e4f7b01abe74c518987c6b44b2c5ea
Guudiass <139389932+Guudiass@users.noreply.github.com>
2025-07-21 20:58:00 -0300

    a

--- scriptMatcher.js
+++
@@ -1,8 +0,0 @@
-// Mapeia hostnames para os arquivos de SCRIPT .JS personalizados no FTP.
-module.exports = {
-    // A chave é o hostname do site.
-    // O valor é o nome do arquivo .js que será baixado e executado.
-    'chat.chatbotapp.ai': 'chatbot_autologin.js',
-    'login.algumsite.com': 'outro_site_script.js',
-    'app.diferente.com': 'ocultar_elementos.js'
-};
